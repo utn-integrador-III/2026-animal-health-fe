2b68b53fe27c4ba8ab9602af48211e83fe67f9cc
Edieth <edloriama@est.utn.ac.cr>
2026-06-26 11:23:44 -0600

    FE-US-02 Pet Registration correction

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,9 +1,20 @@
 export const ROUTES = {
+  PUBLIC: {
+    ABOUT: '/about',
+    SERVICES: '/services',
+    CONTACT: '/contact',
+  },
   AUTH: {
     LOGIN: '/auth/login',
     REGISTER: '/auth/register',
   },
   CLIENT: {
+    DASHBOARD: '/client/pets',
     PETS: '/client/pets',
+    PROFILE: '/client/profile',
+  },
+  VET: {
+    DASHBOARD: '/vet/dashboard',
+    PROFILE: '/vet/profile',
   },
 };
